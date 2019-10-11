ca3ba6b21ec75e4362b2044ca678c75dfb977c18
DovuV <43044056+DovuV@users.noreply.github.com>
2019-10-11 09:56:37 -0500

    Add files via upload

---
+++ pages/lightning/sketch.js
@@ -0,0 +1,82 @@
+'use strict';
+
+var x = 20;
+var y;
+var w = 40;
+var h = 40;
+var speed = 3;
+var velocity = Math.floor((Math.random() * 2) + 1);
+var startX = 0;
+var startY = 200;
+var endX = 0;
+var endY = 200;
+var t = 0;
+var increasing = true;
+
+function setup() {
+    createCanvas(400, 400);
+    strokeWeight(5);
+    y = height / 2;
+}
+
+function draw() {
+    lightning();
+}
+
+function lightning() {
+    background(0);
+    fill(230, 230, 0);
+    ellipse(x, y, w, h);
+    x += speed;
+    y += velocity;
+    if (x > width - w / 2) {
+        if (t <= 0 || increasing) {
+
+            speed *= -1.1;
+            t++;
+            increasing = true;
+        }
+        if (t >= 14 || !increasing) {
+            speed *= -0.9;
+
+            t--;
+            increasing = false;
+        }
+    }
+
+    if (x < 0 + w / 2) {
+        if (t <= 0 || increasing) {
+            speed *= -1.1;
+            t++;
+            increasing = true;
+        }
+        if (t >= 14 || !increasing) {
+          
+            x = 20;
+            speed *= -0.9;
+            t--;
+            increasing = false;
+        }
+    }
+    if (y > height - h / 2) {
+        velocity *= -1;
+    }
+    if (y < 0 + h / 2) {
+        velocity *= -1;
+    }
+    if (x >= width - w / 2 || x <= 0 + w / 2 || y >= height - h / 2 || y <= 0 + h / 2) {
+        stroke((Math.random() * 51) + 200, (Math.random() * 51) + 200, 0);
+        while (endY < height) {
+            endX = startX + (Math.random() * 10);
+            endY = startY + ((Math.random() * 19) - 9);
+            line(startY, startX, endY, endX);
+            startX = endX;
+            startY = endY;
+        }
+    } else {
+        startX = 0;
+        startY = 200;
+        endX = 0;
+        endY = 200;
+    }
+}
